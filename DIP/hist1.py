16db8a8b2692aa899a366a6d626761c9b0898c30
Lpixel00 <53140978+Lpixel00@users.noreply.github.com>
2019-08-01 00:04:33 +0800

    Add files via upload

---
+++ DIP/hist1.py
@@ -0,0 +1,35 @@
+﻿#
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# mat image
+#img=np.zeros((100,100),np.uint8)
+
+#cv2.rectangle(img,(0,50),(100,100),(255),-1)
+#if use -1,
+#if not use -1
+#cv2.circle(img,(50,50),25,127,-1)
+
+#cv2.imshow("img",img)
+
+#plt.hist(img.ravel(),256,[0,256])
+#plt.show()
+
+# gray image
+#img=cv2.imread('road/1.jpg',0)
+
+#plt.hist(img.ravel(),256,[0,256])
+#plt.show()
+
+#bgr image
+img=cv2.imread('road/1.jpg')
+b,g,r=cv2.split(img)
+cv2.imshow("b",b)
+cv2.imshow("g",g)
+cv2.imshow("r",r)
+
+plt.hist(b.ravel(),256,[0,256])
+plt.hist(g.ravel(),256,[0,256])
+plt.hist(r.ravel(),256,[0,256])
+plt.show()
---
+++ DIP/hsv1.py
@@ -0,0 +1,47 @@
+﻿#hsv
+
+import cv2
+import numpy as np
+
+def nothing(x):
+   pass
+
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH","Tracking",0,255,nothing)
+cv2.createTrackbar("LS","Tracking",0,255,nothing)
+cv2.createTrackbar("LV","Tracking",0,255,nothing)
+cv2.createTrackbar("UH","Tracking",0,255,nothing)
+cv2.createTrackbar("US","Tracking",0,255,nothing)
+cv2.createTrackbar("UV","Tracking",0,255,nothing)
+
+
+img=cv2.imread('qiqiu/1.jpg')
+hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+
+l_h=cv2.getTrackbarPos("LH","Tracking")
+print(l_h)
+l_s=cv2.getTrackbarPos("LS","Tracking")
+l_v=cv2.getTrackbarPos("LV","Tracking")
+
+l_b=np.array([l_h,l_s,l_v])
+u_h=cv2.getTrackbarPos("UH","Tracking")
+u_s=cv2.getTrackbarPos("US","Tracking")
+u_v=cv2.getTrackbarPos("UV","Tracking")
+u_b=np.array([u_h,u_s,u_v])
+
+mask=cv2.inRange(hsv,l_b,u_b)
+
+res=cv2.bitwise_and(img,img,mask=mask)
+
+cv2.imwrite('qiqiu/a.jpg',hsv)
+cv2.imwrite('qiqiu/b.jpg',mask)
+cv2.imwrite('qiqiu/c.jpg',res)
+cv2.imshow('hsv',hsv)
+cv2.imshow('mask',mask)
+cv2.imshow('res',res)
+
+
+
+cv2.waitKey(0)#when press any key,it will close the imshow windows
+  
+   
